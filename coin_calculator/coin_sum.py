a59db3c68569e8bebacf8eac3329cefb15ff3c1e
danielginzberg <ginzbeda@gmail.com>
2022-05-10 12:48:37 -0400

    working on it

--- coin_calculator/coin_sum.py
+++ coin_calculator/coin_sum.py
@@ -1,78 +1,102 @@
 import logging
 
 
-# Log current info
+# # Log current info
 import math
 
+# logging.basicConfig(level=# logging.DEBUG)
 
-def log(coins, coin_index, remaining):
-    logging.debug(coins[coin_index], " at: ", coin_index, " remaining : ", remaining)
+
+# def log(coins, coin_index, remaining):
+    # logging.debug('%s at: %s remaining: %d', coins[coin_index], coin_index, remaining)
 
 
 # pick next middle
 def pick_middle(start, end):
-    logging.info("new middle: ", start + (end - start)//2)
+    # logging.info("new middle: %d", start + (end - start)//2)
+    # ends of list
+    if end - start == 1:
+        if start == 0:
+            return start
+        return end
+    # even try for higher
     if (end - start)%2 > 0:
-        return (start + (end - start) // 2) + 1
-    else:
         return start + (end - start) // 2
+    # odd
+    else:
+        return (start + (end - start) // 2) + 1
 
 
 # once coin is picked as next it is added to used coins, coin count and remaining updated
 def add_coin(coins, coin_index, remaining, coin_count, result_coins):
-    logging.info("coin added")
-    log(coins, coin_index, remaining)
-    result_coins.append(coins[coin_index])
-    return remaining - coin_index, coin_count + 1, result_coins
+    # logging.info("coin added")
+    # log(coins, coin_index, remaining)
+    # already found greatest coin
+    # use as many times as possible
+    while remaining >= coins[coin_index]:
+        result_coins.append(coins[coin_index])
+        coin_count = coin_count + 1
+        remaining = remaining - coins[coin_index]
+    return pick_middle(0, len(coins)-1), remaining, coin_count, result_coins
 
 
 # move index to next likely coin
 def index_move(coins, coin_index, remaining):
-    logging.info("index move")
-    log(coins, coin_index, remaining)
-    right = coins[coin_index+1]
-    left = coins[coin_index-1]
-    right_remaining = remaining - right
-    left_remaining = remaining - left
-    curr_remaining = remaining  - coins[coin_index]
-    if right_remaining == 0:
-        return coin_index + 1
-    elif remaining - left == 0:
-        return coin_index - 1
-    elif curr_remaining < 0:
-        if abs(left_remaining) <= abs(right_remaining) and abs(left_remaining) <= remaining - coins[coin_index]:
+    # logging.info("index move")
+    # log(coins, coin_index, remaining)
+    if not is_end(coin_index, coins):
+        right_remaining = remaining - coins[coin_index+1]
+        left_remaining = remaining - coins[coin_index-1]
+        curr_remaining = remaining - coins[coin_index]
+        if right_remaining == 0:
+            return coin_index + 1
+        elif left_remaining == 0:
+            return coin_index - 1
+        elif curr_remaining == 0:
+            return coin_index
+        #if current
+        # if curr_remaining <= remaining or left_remaining > remaining:
+        #     if abs(left_remaining) <= abs(right_remaining) \
+        #             and abs(left_remaining) <= abs(remaining - coins[coin_index]):
+        #         return pick_middle(0, coin_index)
+        #     elif abs(right_remaining) <= abs(left_remaining) \
+        #             and abs(right_remaining) <= abs(remaining - coins[coin_index]):
+        #         return pick_middle(0, coin_index)
+        elif (right_remaining <= left_remaining) \
+                and (right_remaining <= remaining - coins[coin_index]):
+            return pick_middle(coin_index, len(coins)-1)
+        elif (left_remaining <= right_remaining) \
+                and (left_remaining <= remaining - coins[coin_index]):
             return pick_middle(0, coin_index)
-        elif abs(right_remaining) <= abs(left_remaining) and abs(right_remaining) <= remaining - coins[coin_index]:
-            return pick_middle(0, coin_index)
-    elif (0 <= abs(right_remaining) <= abs(left_remaining)) \
-            and (abs(right_remaining <= remaining) - coins[coin_index]):
-        return pick_middle(coin_index, len(coins)-1)
-    elif (0 <= left_remaining <= right_remaining) \
-            and (abs(remaining - left) <= remaining - coins[coin_index]):
-        return pick_middle(0, coin_index)
     else:
         return coin_index
 
+def is_end(coin_index, coins):
+    if coin_index == 0 or coin_index == len(coins)-1:
+        return True
+    return False
+
 
 # Coins to reach sum goal
-def coin_sum(all_coins, goal_sum):
+def coin_sum(coins, goal_sum):
     coin_index = 0
-    coins = all_coins
     remaining = goal_sum
     result_coins = []
     coin_count = 0
-
+    # logging.info(coins)
     # start in the middle in case a lower coin is better
     coin_index = pick_middle(coin_index, len(coins))
-    log(coins, coin_index, remaining)
+    # log(coins, coin_index, remaining)
     # while has remaining and smallest coin is not too big
-    while remaining != 0 and coins[0] < remaining:
+    while remaining != 0 \
+            and coins[0] < remaining:
         prev = coin_index
         coin_index = index_move(coins, coin_index, remaining)
         coin = coins[coin_index]
 
         # if current index is best or is remaining
-        if prev == coin_index or coin == remaining:
-            remaining, coin_count, result_coins = add_coin(coins, coin_index, remaining, coin_count, result_coins)
+        if prev == coin_index or coin == remaining or is_end(coin_index, coins):
+            coin_index, remaining, coin_count, result_coins = \
+                add_coin(coins, coin_index, remaining, coin_count, result_coins)
 
-
+    return coin_count, result_coins
--- tests/test_coin_sum.py
+++ tests/test_coin_sum.py
@@ -10,54 +10,86 @@ class Test(unittest.TestCase):
         remaining = 10
         coin_count = 0
         result_coins = []
-        expected = 8, 1, [3]
-        self.assertEqual(add_coin(coins, coin_index, remaining, coin_count, result_coins), expected)
+        expected = 2, 1, 3, [3, 3, 3]
+        self.assertEqual(expected, add_coin(coins, coin_index, remaining, coin_count, result_coins))
+
+    def test_pick_end(self):
+        self.assertEqual(10, pick_middle(9, 10))
+
+    def test_pick_start(self):
+        self.assertEqual(0, pick_middle(0, 1))
 
     def test_pick_middle_odd(self):
         start = 0
         end = 4
-        self.assertEquals(pick_middle(start, end), 2)
+        self.assertEqual(2, pick_middle(start, end))
 
     def test_pick_middle_even(self):
         start = 0
         end = 5
-        self.assertEquals(pick_middle(start, end), 3)
+        self.assertEqual(3, pick_middle(start, end))
 
     def test_pick_middle_right(self):
         start = 3
         end = 7
-        self.assertEquals(pick_middle(start, end), 5)
+        self.assertEqual(5, pick_middle(start, end));
 
     def test_index_move_on_right(self):
         coins = [1, 2, 3, 4, 5]
         coin_index = 2
         remaining = 4
-        self.assertEqual(index_move(coins, coin_index, remaining), 3)
+        self.assertEqual(3, index_move(coins, coin_index, remaining))
 
     def test_index_move_on_left(self):
         coins = [1, 2, 3, 4, 5]
         coin_index = 2
         remaining = 2
-        self.assertEqual(index_move(coins, coin_index, remaining), 1)
+        self.assertEqual(1, index_move(coins, coin_index, remaining))
 
     def test_index_move_to_right(self):
         coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         coin_index = 4
         remaining = 9
-        self.assertEqual(index_move(coins, coin_index, remaining), 7)
+        self.assertEqual(7, index_move(coins, coin_index, remaining))
 
     def test_index_move_to_left(self):
         coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         coin_index = 4
         remaining = 2
-        self.assertEqual(index_move(coins, coin_index, remaining), 2)
+        self.assertEqual(2, index_move(coins, coin_index, remaining))
 
     def test_index_move_on(self):
         coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         coin_index = 4
         remaining = 5
-        self.assertEqual(index_move(coins, coin_index, remaining), 4)
+        self.assertEqual(4, index_move(coins, coin_index, remaining))
+
+    def test_coin_sum1(self):
+        coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        goal_sum = 22
+        self.assertEqual((3, [10, 10, 2]), coin_sum(coins, goal_sum))
+
+    def test_coin_sum2(self):
+        coins = [23]
+        goal_sum = 22
+        self.assertEqual((0, []), coin_sum(coins, goal_sum))
+
+    def test_coin_sum3(self):
+        coins = [-1, -2, -3]
+        goal_sum = 22
+        self.assertEqual((0, []), coin_sum(coins, goal_sum))
+
+    def test_coin_sum4(self):
+        coins = [2, 2, 2]
+        goal_sum = 10
+        self.assertEqual((5, [2, 2, 2, 2, 2]), coin_sum(coins, goal_sum))
+
+    def test_coin_sum5(self):
+        coins = [1, 2]
+        goal_sum = 5
+        self.assertEqual((3, [2, 2, 1]), coin_sum(coins, goal_sum))
 
 
 if __name__ == '__main__':
     unittest.main()
+    logging.basicConfig(level=logging.DEBUG)
